8cffd28a079f8fc5538f678252a9fee1a98abe03
Shashank <s_shashank@outlook.com>
2019-12-24 23:33:24 +0530

    created routes

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp with id ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamp", (req, res) => {
+  res.status(201).json({ success: true, msg: "Bootcamp created" });
+});
+
+app.put("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(201)
+    .json({ success: true, msg: `Bootcamp with id ${req.params.id} updated` });
+});
+
+app.delete("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Bootcamp ${req.params.id} deleted` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
