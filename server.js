d263b8ff7ef69dded6e0ae169a23d316fdd6b461
Shashank <s_shashank@outlook.com>
2019-12-25 17:39:32 +0530

    database connection to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -20,9 +23,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
   )
 );
+
+// Handle unhandled rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit
+  server.close(() => process.exit(1));
+});
