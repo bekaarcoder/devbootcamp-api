e40037f71edc57b1ccccf6f82c647bbf863912a1
Shashank <s_shashank@outlook.com>
2020-01-03 21:36:37 +0530

    role authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,23 +13,25 @@ const courseRouter = require("./courses");
 
 const router = express.Router();
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resource router
 router.use("/:bootcampId/courses", courseRouter);
 
 // route for photo upload
-router.route("/:id/photo").put(protect, uploadPhoto);
+router
+  .route("/:id/photo")
+  .put(protect, authorize("publisher", "admin"), uploadPhoto);
 
 router
   .route("/")
   .get(getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
 
 router
   .route("/:id")
   .get(viewBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -8,17 +8,17 @@ const {
 } = require("../controllers/courses");
 const router = express.Router({ mergeParams: true });
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 router
   .route("/")
   .get(getCourses)
-  .post(protect, addCourse);
+  .post(protect, authorize("publisher", "admin"), addCourse);
 
 router
   .route("/:id")
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse);
+  .put(protect, authorize("publisher", "admin"), updateCourse)
+  .delete(protect, authorize("publisher", "admin"), deleteCourse);
 
 module.exports = router;
